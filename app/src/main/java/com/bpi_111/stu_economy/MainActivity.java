b697d84b05a36fb450b0eceb3b9671053a4993a0
Danila <eremencko.danila2005@gmail.com>
2023-10-24 23:10:08 +0700

    Initial commit

---
+++ app/src/main/java/com/bpi_111/stu_economy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bpi_111.stu_economy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
